27bdee030027b470f5d00d6740d84571463a77dd
Anton Lisitsyn <lisitsyn.anton.a@gmail.com>
2017-10-23 18:31:12 +0300

    download project to github

---
+++ CitySearcher/City.swift
@@ -0,0 +1,21 @@
+//
+//  City.swift
+//  CitySearcher
+//
+//  Created by Anton on 21.10.17.
+//  Copyright © 2017 Anton. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class City {
+    var name: String?
+    var country: String?
+    
+    init (json: JSON) {
+        self.name = json["city"].stringValue
+        self.country = json["country"].stringValue
+    }
+    public init() {}
+}
